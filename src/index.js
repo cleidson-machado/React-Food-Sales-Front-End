b415e00f8f2457bbfebe16dd1a4aaea50a94cbfd
Cleidson <cleidson.mac@outlook.com>
2024-04-09 08:29:25 -0400

    Revert "Add Default Routes and  Menu Behaivor plus Css and Icons 2"
    This reverts commit 828e83d8bd3f77969012c5e2a3b92702c49357cd, reversing
    changes made to 142a827a34dd4cbd4af387f93cf32a76d4c15dd0.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-<<<<<<< HEAD
 import NavMenuCSS1 from "./core/components/NavMenuCSS1";
-=======
-import NavMenu from "./core/components/NavMenu";
->>>>>>> a60bfc315d5ca54a2af7b052a75c51646744275e
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <NavMenuCSS1 />
-=======
-      <NavMenu />
->>>>>>> a60bfc315d5ca54a2af7b052a75c51646744275e
       <App />
     </BrowserRouter>
   </React.StrictMode>
