075d46f612bfdf2435244e6f069bb79b4ebb8204
Cleidson <cleidson.mac@outlook.com>
2024-04-10 20:18:06 -0400

    Just Add a Products Page and Route

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Pizza from "./products/pizza/index";
 import NotFound from "./404-notFound/index";
 import { Route, Routes } from "react-router-dom";
 import HelpInfo from "./help-info";
+import Products from "./products/all-of-them/index";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/products/" element={<Products />} />
       <Route path="/products/pizza" element={<Pizza />} />
       <Route path="/products/pizza/:id" element={<Pizza />} />
       <Route path="/help-info" element={<HelpInfo />} />
